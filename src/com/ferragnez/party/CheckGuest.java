26167522941987e3f127e68691e95941ef772f60
FabrettiMarco <mfabretti2000@gmail.com>
2021-11-19 15:14:25 +0100

    Initial commit

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,26 @@
+package com.ferragnez.party;
+import java.util.Scanner;
+public class CheckGuest {
+
+	public static void main(String[] args) {
+		String[] lista = {"Dua Lipa","Paris Hilton","Manuel Agnelli","J-Ax","Francesco Totti","Ilary Blasi","Babe Vio","Luis","Paradis Zarei","Martina Maccherone","Rachel Zeilic"};
+		String nome;
+		boolean trovato=false;
+		Scanner cIn = new Scanner(System.in);
+		System.out.println("Inseririsci il tuo nome, per verificare se sei sulla lista: ");
+		nome=cIn.nextLine();
+		
+		
+		
+		for(int i=0;i<lista.length;i++){
+			if(lista[i].equals(nome)) {
+				trovato=true;
+				}			
+		}
+		if(trovato==true) {
+			System.out.println("Benvenuto alla festa");
+		}else System.out.println("Mi dispiace ma non sei sulla lista");
+		cIn.close();
+	}
+
+}
