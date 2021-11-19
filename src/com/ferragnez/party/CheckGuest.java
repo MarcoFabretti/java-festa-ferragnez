fdcdb0aa8da792c246b3a2df2c8e39697355765f
FabrettiMarco <mfabretti2000@gmail.com>
2021-11-19 15:15:41 +0100

    ops

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -9,16 +9,13 @@ public class CheckGuest {
 		Scanner cIn = new Scanner(System.in);
 		System.out.println("Inseririsci il tuo nome, per verificare se sei sulla lista: ");
 		nome=cIn.nextLine();
-		
-		
-		
 		for(int i=0;i<lista.length;i++){
 			if(lista[i].equals(nome)) {
 				trovato=true;
 				}			
 		}
 		if(trovato==true) {
-			System.out.println("Benvenuto alla festa");
+			System.out.println("Benvenuto/a alla festa");
 		}else System.out.println("Mi dispiace ma non sei sulla lista");
 		cIn.close();
 	}
